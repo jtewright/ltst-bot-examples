8206256a8ea57df7c9c51b05325c496fbf83772e
Joseph Wright <jtewright@gmail.com>
2022-02-27 22:05:18 +0000

    Create randomcolour.js

---
+++ examples/randomcolour.js
@@ -0,0 +1,19 @@
+// https://ltst.xyz/channel/323042008542216784
+function pickTextColor(bgColor) {
+  const color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  const r = parseInt(color.substring(0, 2), 16); // hexToR
+  const g = parseInt(color.substring(2, 4), 16); // hexToG
+  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  const uicolors = [r / 255, g / 255, b / 255];
+  const c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  const L = (0.2126 * c[0]) + (0.7152 * c[1]) + (0.0722 * c[2]);
+  return (L > 0.179) ? '#000000' : '#E5E7EB';
+}
+const hex = Math.floor(Math.random()*16777215).toString(16);
+const bgColor = '#' + hex;
+postUpdate({text: bgColor, bgColor, textColor: pickTextColor(bgColor)});
